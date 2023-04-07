11f8807de097ffff1b95c8f0f0b281c16e1b76de
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-07 10:01:02 -0300

    debug

--- routes/dalle.routes.js
+++ routes/dalle.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import * as dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from 'openai';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi} from 'openai';
 
 dotenv.config();
 
@@ -13,13 +13,13 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES"})
-});
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
 
 router.route('/').post(async (req, res) => {
-  try{
+  try {
     const { prompt } = req.body;
-    
+
     const response = await openai.createImage({
       prompt,
       n: 1,
@@ -30,10 +30,9 @@ router.route('/').post(async (req, res) => {
     const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Oops, something went wrong" })
+    res.status(500).json({ message: "Something went wrong" })
   }
 })
 
